75265b4e6052b839a5d12e9ea150e6a8771cb0d9
彭智鑫 <pengzhixin@lizhi.fm>
2021-03-25 15:27:58 +0800

    Initial Commit

---
+++ weibo/weiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  weiboApp.swift
+//  weibo
+//
+//  Created by 彭智鑫 on 2021/3/25.
+//
+
+import SwiftUI
+
+@main
+struct weiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
